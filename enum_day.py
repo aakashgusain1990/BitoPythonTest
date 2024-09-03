166b487cebf9e91a9cee59e94e5270c8db0ec76e
Akash Gusain <akashgusain@Akashs-MacBook-Pro.local>
2024-09-03 14:31:21 +0530

    Test enum changes.

---
+++ enum_day.py
@@ -0,0 +1,11 @@
+from calendar import SATURDAY, THURSDAY, WEDNESDAY
+from enum import Enum
+
+class Day(Enum):
+    SUNDAY = 1
+    MONDAY = 2
+    TUESDAY = 3
+    WEDNESDAY = 4
+    THURSDAY = 5
+    FRIDAY = 6
+    SATURDAY = 7
---
+++ main.py
@@ -0,0 +1,8 @@
+from enum_day import Day
+
+
+def read_enum():
+    print(Day.FRIDAY.name)
+
+if __name__=="__main__":
+    read_enum()
